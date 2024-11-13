55b9a4697474c6e46210cc95819c250884d1524f
Arash Khoeini <arash.khoeini@gmail.com>
2024-11-13 12:15:51 -0800

    bug fix

--- main_pretrain.py
+++ main_pretrain.py
@@ -42,11 +42,59 @@ def main_worker(configs):
     
 
 def main():
-    parser = argparse.ArgumentParser(description='Upper level params')
-    # parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
+    parser = argparse.ArgumentParser(description='PyTorch Project Configurations')
+
+    # General Configurations
+    parser.add_argument('--verbose', default=True, type=bool, help='enable verbose logging')
+    parser.add_argument('--cuda', default=False, type=bool, help='use CUDA if available')
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
     parser.add_argument('--rank', default=0, type=int, help='rank of the current process')
-    parser.add_argument('--resume', default='', type=str, help='path to latest checkpoint')
-    parser.add_argument('--dataset.root', default='', type=str, help='path to data root')
+    parser.add_argument('--seed', default=1, type=int, help='random seed for reproducibility')
+
+    # WandB Configurations
+    parser.add_argument('--wandb.enable', default=False, type=bool, help='enable Weights & Biases logging')
+    parser.add_argument('--wandb.project', default='resnet50-infonce-imagenet', type=str, help='Weights & Biases project name')
+    parser.add_argument('--wandb.key', default='7047c70c12cc7631dfdbc7f66f14b27e9d06c71d', type=str, help='Weights & Biases API key')
+
+    # Dataset Configuration
+    parser.add_argument('--dataset.name', default='miniimagenet_toy', type=str, help='name of the dataset')
+    parser.add_argument('--dataset.root', default='$SLURM_TMPDIR/data', type=str, help='path to dataset root directory')
+    parser.add_argument('--dataset.ood', default=['cub200_toy/images'], type=list, help='list of out-of-distribution datasets')
+    parser.add_argument('--dataset.batch_size', default=64, type=int, help='batch size for training and validation')
+
+    # Model Configuration
+    parser.add_argument('--model.method', default='supervised_infonce', choices=['supervised_infonce', 'infonce', 'mocov2'],type=str, help='training method (e.g., mocov2 or infonce)')
+    parser.add_argument('--model.encoder', default='resnet50', type=str, help='encoder architecture')
+    parser.add_argument('--model.checkpoint_source', default='microsoft', choices=['microsoft', 'leftthomas', 'francesco'], type=str, help='source for pre-trained checkpoints')
+    parser.add_argument('--model.temperature', default=0.07, type=float, help='temperature parameter for contrastive learning')
+    parser.add_argument('--model.feature_dim', default=128, type=int, help='dimensionality of the feature embedding')
+    parser.add_argument('--model.num_classes', default=100, type=int, help='number of classes in the dataset')
+    parser.add_argument('--model.input_size', default=[3, 32, 32], type=list, help='input image dimensions')
+    parser.add_argument('--model.pretrained', default=True, type=bool, help='use pretrained model weights')
+
+    # Pretraining Configuration
+    parser.add_argument('--pretraining.epochs', default=1, type=int, help='number of pretraining epochs')
+    parser.add_argument('--pretraining.warmup_epochs', default=1, type=int, help='number of warmup epochs')
+    parser.add_argument('--pretraining.optimizer', default='lamb', type=str, choices=['lamb', 'sgd'],  help='optimizer type (e.g., lamb or std)')
+    parser.add_argument('--pretraining.learning_rate', default=0.001, type=float, help='learning rate for optimizer')
+    parser.add_argument('--pretraining.weight_decay', default=0.0001, type=float, help='weight decay for optimizer')
+    parser.add_argument('--pretraining.momentum', default=0.9, type=float, help='momentum for optimizer')
+    parser.add_argument('--pretraining.checkpoint_freq', default=1, type=int, help='frequency of checkpoint saving')
+    parser.add_argument('--pretraining.accumulation_steps', default=1, type=int, help='number of gradient accumulation steps')
+
+    # Training Configuration
+    parser.add_argument('--training.epochs', default=1, type=int, help='number of training epochs')
+    parser.add_argument('--training.warmup_epochs', default=1, type=int, help='number of warmup epochs')
+    parser.add_argument('--training.learning_rate', default=0.001, type=float, help='learning rate for training')
+    parser.add_argument('--training.weight_decay', default=0.0001, type=float, help='weight decay for training optimizer')
+    parser.add_argument('--training.momentum', default=0.9, type=float, help='momentum for training optimizer')
+    parser.add_argument('--training.checkpoint_freq', default=1, type=int, help='frequency of checkpoint saving during training')
+
+    # Logging Configuration
+    parser.add_argument('--logging.log_dir', default='/path/to/logs', type=str, help='directory to save log files')
+    parser.add_argument('--logging.tensorboard', default=True, type=bool, help='enable TensorBoard logging')
+    parser.add_argument('--logging.checkpoint', default=True, type=bool, help='enable checkpoint saving')
+    
     args = parser.parse_args()
     
     if args.resume:
